849f43b133e0d8777d5c906e3cb450f26ea29904
tgdwyer <tgdwyer@gmail.com>
2020-09-19 20:48:01 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -7,44 +7,46 @@ Observables allow us to capture asynchronous actions like user interface events 
 As an example we will build a little "Asteroids" game using Observables.  We're going to use [rxjs](https://rxjs-dev.firebaseapp.com/) as our Observable implementation, and we are going to render it in HTML using SVG.
 We're also going to take some pains to make pure functional code (and lots of beautiful curried lambda (arrow) functions). We'll use [typescript type annotations](https://www.typescriptlang.org/) to help us ensure that our data is indeed immutable and to guide us in plugging everything together without type errors.
  */
-import { fromEvent,interval } from 'rxjs'; 
-import { map,filter,merge,scan } from 'rxjs/operators';
+import { fromEvent, interval, merge } from 'rxjs'; 
+import { map, filter, scan } from 'rxjs/operators';
 
-const 
-  Constants = new class {
-    readonly CanvasSize = 600;
-    readonly BulletExpirationTime = 1000;
-    readonly BulletRadius = 3;
-    readonly BulletVelocity = 2;
-    readonly StartRockRadius = 30;
-    readonly StartRocksCount = 5;
-    readonly RotationAcc = 0.1;
-    readonly ThrustAcc = 0.1;
-    readonly StartTime = 0;
-  },
-  torusWrap = ({x,y}:Vec) => { 
-    const s=Constants.CanvasSize, 
-      wrap = (v:number) => v < 0 ? v + s : v > s ? v - s : v;
-    return new Vec(wrap(x),wrap(y))
-  };
-  
 type Key = 'ArrowLeft' | 'ArrowRight' | 'ArrowUp' | 'Space'
 type Event = 'keydown' | 'keyup'
-// our game has the following view element types:
-type ViewType = 'ship' | 'rock' | 'bullet'
 
 function asteroids() {
+  const 
+    Constants = {
+      CanvasSize: 600,
+      BulletExpirationTime: 1000,
+      BulletRadius: 3,
+      BulletVelocity: 2,
+      StartRockRadius: 30,
+      StartRocksCount: 5,
+      RotationAcc: 0.1,
+      ThrustAcc: 0.1,
+      StartTime: 0
+    } as const
+
+  // our game has the following view element types:
+  type ViewType = 'ship' | 'rock' | 'bullet'
+
+  // Four types of game state transitions
   class Tick { constructor(public readonly elapsed:number) {} }
   class Rotate { constructor(public readonly direction:number) {} }
   class Thrust { constructor(public readonly on:boolean) {} }
   class Shoot { constructor() {} }
   
-  const keyObservable = <T>(e:Event, k:Key, result:()=>T)=>
-    fromEvent<KeyboardEvent>(document,e)
+  const 
+    gameClock = interval(10)
+      .pipe(map(elapsed=>new Tick(elapsed))),
+
+    keyObservable = <T>(e:Event, k:Key, result:()=>T)=>
+      fromEvent<KeyboardEvent>(document,e)
         .pipe(
           filter(({code})=>code === k),
           filter(({repeat})=>!repeat),
           map(result)),
+
     startLeftRotate = keyObservable('keydown','ArrowLeft',()=>new Rotate(-.1)),
     startRightRotate = keyObservable('keydown','ArrowRight',()=>new Rotate(.1)),
     stopLeftRotate = keyObservable('keyup','ArrowLeft',()=>new Rotate(0)),
@@ -53,6 +55,7 @@ function asteroids() {
     stopThrust = keyObservable('keyup','ArrowUp', ()=>new Thrust(false)),
     shoot = keyObservable('keydown','Space', ()=>new Shoot())
 
+  // Every object that participates in physics is a Body
   type Body = Readonly<{
     id:string,
     viewType: ViewType,
@@ -65,6 +68,8 @@ function asteroids() {
     radius:number,
     createTime:number
   }>
+
+  // Game state
   type State = Readonly<{
     time:number,
     ship:Body,
@@ -74,6 +79,7 @@ function asteroids() {
     objCount:number,
     gameOver:boolean
   }>
+
   // Rocks and bullets are both just circles
   const createCircle = (viewType: ViewType)=> (oid:number)=> (time:number)=> (radius:number)=> (pos:Vec)=> (vel:Vec)=>
     <Body>{
@@ -85,7 +91,8 @@ function asteroids() {
       radius: radius,
       id: viewType+oid,
       viewType: viewType
-    };
+    }
+
   function createShip():Body {
     return {
       id: 'ship',
@@ -100,11 +107,13 @@ function asteroids() {
       createTime:0
     }
   }
+
   const
     startRocks = [...Array(Constants.StartRocksCount)]
       .map((_,i)=>createCircle("rock")(i)
          (Constants.StartTime)(Constants.StartRockRadius)(Vec.Zero)
          (new Vec(0.5 - Math.random(), 0.5 - Math.random()))),
+
     initialState:State = {
       time:0,
       ship: createShip(),
@@ -114,78 +123,109 @@ function asteroids() {
       objCount: Constants.StartRocksCount,
       gameOver: false
     },
-    moveObj = (o:Body) => <Body>{
+
+    // wrap a positions around edges of the screen
+    torusWrap = ({x,y}:Vec) => { 
+      const s=Constants.CanvasSize, 
+        wrap = (v:number) => v < 0 ? v + s : v > s ? v - s : v;
+      return new Vec(wrap(x),wrap(y))
+    },
+
+    // all movement comes through here
+    moveBody = (o:Body) => <Body>{
       ...o,
       rotation: o.rotation + o.torque,
       angle:o.angle+o.rotation,
       pos:torusWrap(o.pos.add(o.vel)),
       vel:o.vel.add(o.acc)
     },
+    
+    // check a State for collisions:
+    //   bullets destroy rocks spawning smaller ones
+    //   ship colliding with rock ends game
     handleCollisions = (s:State) => {
       const
-        bodiesCollided = (a:Body,b:Body) => a.pos.sub(b.pos).len() < a.radius + b.radius,
-        elem = (a:Body[]) => (e:Body) => a.findIndex(b=>b.id === e.id) >= 0,
-        shipCollided = s.rocks.filter(r=>bodiesCollided(s.ship,r)).length > 0,
-        allBulletsAndRocks = flatMap(s.bullets,b=>s.rocks.map(r=>({bullet:b,rock:r}))),
-        collidedBulletsAndRocks = allBulletsAndRocks.filter(({bullet:b,rock:r})=>bodiesCollided(b,r)),
-        collidedBullets = collidedBulletsAndRocks.map(({bullet})=>bullet),
-        collidedRocks = collidedBulletsAndRocks.map(({rock})=>rock),
-        createChildRock = (r:Body,dir:number)=>
-          createCircle('rock')(0/*we assign the ids later*/)
-                    (s.time)(r.radius/2)
-                    (r.pos)(r.vel.ortho().scale(dir)),
-        spawnChildRocks = (r:Body)=>
+        bodiesCollided = ([a,b]:[Body,Body]) => a.pos.sub(b.pos).len() < a.radius + b.radius,
+        shipCollided = s.rocks.filter(r=>bodiesCollided([s.ship,r])).length > 0,
+        allBulletsAndRocks = flatMap(s.bullets, b=> s.rocks.map(r=>([b,r]))),
+        collidedBulletsAndRocks = allBulletsAndRocks.filter(bodiesCollided),
+        collidedBullets = collidedBulletsAndRocks.map(([bullet,_])=>bullet),
+        collidedRocks = collidedBulletsAndRocks.map(([_,rock])=>rock),
+
+        // spawn two children for each collided rock above a certain size
+        child = (r:Body,dir:number)=>({
+          radius: r.radius/2,
+          pos:r.pos,
+          vel:r.vel.ortho().scale(dir)
+        }),
+        spawnChildren = (r:Body)=>
                               r.radius >= Constants.StartRockRadius/4 
-                              ? [createChildRock(r,1),createChildRock(r,-1)] : [],
-        newRocks = flatMap(collidedRocks, spawnChildRocks)
-          .map((r,i)=><Body>{...r, id: r.viewType + (s.objCount + i)})
+                              ? [child(r,1), child(r,-1)] : [],
+        newRocks = flatMap(collidedRocks, spawnChildren)
+          .map((r,i)=>createCircle('rock')(s.objCount + i)(s.time)(r.radius)(r.pos)(r.vel)),
+
+        // search for a body by id in an array
+        elem = (a:ReadonlyArray<Body>) => (e:Body) => a.findIndex(b=>b.id === e.id) >= 0,
+        // array a except anything in b
+        except = (a:ReadonlyArray<Body>) => (b:Body[]) => a.filter(not(elem(b)))
+      
       return <State>{
         ...s,
-        bullets: s.bullets.filter(not(elem(collidedBullets))),
-        rocks: s.rocks.filter(not(elem(collidedRocks))).concat(newRocks),
+        bullets: except(s.bullets)(collidedBullets),
+        rocks: except(s.rocks)(collidedRocks).concat(newRocks),
         exit: s.exit.concat(collidedBullets,collidedRocks),
         objCount: s.objCount + newRocks.length,
         gameOver: shipCollided
       }
     },
+
+    // interval tick: bodies move, bullets expire
     tick = (s:State,elapsed:number) => {
-    const 
-      expired = (b:Body)=>(elapsed - b.createTime) > 100,
-      expiredBullets:Body[] = s.bullets.filter(expired),
-      activeBullets = s.bullets.filter(not(expired));
-    return handleCollisions({...s, 
-      ship:moveObj(s.ship), 
-      bullets:activeBullets.map(moveObj), 
-      rocks: s.rocks.map(moveObj),
-      exit:expiredBullets,
-      time:elapsed
-    })
-  },
-  reduceState = (s:State, e:Rotate|Thrust|Tick|Shoot)=>
-    e instanceof Rotate ? {...s,
-      ship: {...s.ship,torque:e.direction}
-    } :
-    e instanceof Thrust ? {...s,
-      ship: {...s.ship, acc:e.on?Vec.unitVecInDirection(s.ship.angle).scale(Constants.ThrustAcc):Vec.Zero}
-    } :
-    e instanceof Shoot ? {...s,
-      bullets: s.bullets.concat([
-            ((unitVec:Vec)=>
-              createCircle('bullet')(s.objCount)(s.time)
-                (Constants.BulletRadius)
-                (s.ship.pos.add(unitVec.scale(s.ship.radius)))
-                (s.ship.vel.add(unitVec.scale(Constants.BulletVelocity))))(Vec.unitVecInDirection(s.ship.angle))]),
-      objCount: s.objCount + 1
-    } : 
-    tick(s,e.elapsed);
-  const subscription = interval(10).pipe(
-    map(elapsed=>new Tick(elapsed)),
-    merge(
-      startLeftRotate,startRightRotate,stopLeftRotate,stopRightRotate),
-    merge(startThrust,stopThrust),
-    merge(shoot),
-    scan(reduceState, initialState)
-    ).subscribe(updateView);
+      const 
+        expired = (b:Body)=>(elapsed - b.createTime) > 100,
+        expiredBullets:Body[] = s.bullets.filter(expired),
+        activeBullets = s.bullets.filter(not(expired));
+      return handleCollisions({...s, 
+        ship:moveBody(s.ship), 
+        bullets:activeBullets.map(moveBody), 
+        rocks: s.rocks.map(moveBody),
+        exit:expiredBullets,
+        time:elapsed
+      })
+    },
+
+    // state transducer
+    reduceState = (s:State, e:Rotate|Thrust|Tick|Shoot)=>
+      e instanceof Rotate ? {...s,
+        ship: {...s.ship,torque:e.direction}
+      } :
+      e instanceof Thrust ? {...s,
+        ship: {...s.ship, acc:e.on?Vec.unitVecInDirection(s.ship.angle).scale(Constants.ThrustAcc):Vec.Zero}
+      } :
+      e instanceof Shoot ? {...s,
+        bullets: s.bullets.concat([
+              ((unitVec:Vec)=>
+                createCircle('bullet')(s.objCount)(s.time)
+                  (Constants.BulletRadius)
+                  (s.ship.pos.add(unitVec.scale(s.ship.radius)))
+                  (s.ship.vel.add(unitVec.scale(Constants.BulletVelocity))))(Vec.unitVecInDirection(s.ship.angle))]),
+        objCount: s.objCount + 1
+      } : 
+      tick(s,e.elapsed)
+
+  // main game stream
+  const subscription =
+    merge(gameClock,
+      startLeftRotate,startRightRotate,
+      stopLeftRotate,stopRightRotate,
+      startThrust,stopThrust,
+      shoot)
+    .pipe(
+      scan(reduceState, initialState))
+    .subscribe(updateView)
+
+  // Update the svg scene.  
+  // This is the only impure function in this program
   function updateView(s: State) {
     const 
       svg = document.getElementById("svgCanvas")!,
@@ -196,7 +236,7 @@ function asteroids() {
       attr = (e:Element,o:Object) =>
         { for(const k in o) e.setAttribute(k,String(o[k])) },
       updateBodyView = (b:Body) => {
-        const createBodyView = ()=>{
+        function createBodyView() {
           const v = document.createElementNS(svg.namespaceURI, "ellipse")!;
           attr(v,{id:b.id,rx:b.radius,ry:b.radius});
           v.classList.add(b.viewType)
@@ -214,7 +254,16 @@ function asteroids() {
     s.rocks.forEach(updateBodyView);
     s.exit.map(o=>document.getElementById(o.id))
           .filter(isNotNullOrUndefined)
-          .forEach(v=>svg.removeChild(v))
+          .forEach(v=>{
+            try {
+              svg.removeChild(v)
+            } catch(e) {
+              // rarely it can happen that a bullet can be in exit 
+              // for both expiring and colliding in the same tick,
+              // which will cause this exception
+              console.log("Already removed: "+v.id)
+            }
+          })
     if(s.gameOver) {
       subscription.unsubscribe();
       const v = document.createElementNS(svg.namespaceURI, "text")!;
@@ -266,7 +315,10 @@ class Vec {
  * @param a an array
  * @param f a function that produces an array
  */
-function flatMap<T,U>(a:ReadonlyArray<T>,f:(a:T)=>U[]):ReadonlyArray<U> {
+function flatMap<T,U>(
+  a:ReadonlyArray<T>,
+  f:(a:T)=>ReadonlyArray<U>
+): ReadonlyArray<U> {
   return Array.prototype.concat(...a.map(f));
 }
 
